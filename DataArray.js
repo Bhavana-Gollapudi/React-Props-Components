86b85452d542ad1942e229c8b11aa89cb02934b4
Bhavana Gollapudi <69720762+Bhavana-Gollapudi@users.noreply.github.com>
2020-10-26 14:23:22 +0530

    Create DataArray.js

---
+++ DataArray.js
@@ -0,0 +1,22 @@
+const dataArray = [
+    {
+        id: 1,
+        name: "Bhavs",
+        age: "23",
+        email: "bhavs@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Rodney",
+        age: "25",
+        email: "rodney@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Andrea",
+        age: "33",
+        email: "andrea@gmail.com"
+    }
+]
+
+export default dataArray;
